154a891c0675afe4a2a01749fc32ceb9155a2387
laoniu-meow <laoniu.meow@gmail.com>
2025-08-31 21:23:41 +0800

    chore: sync workspace

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,7 @@ module.exports = {
     es2021: true,
     commonjs: true
   },
-  extends: [
-    'eslint:recommended'
-  ],
+  extends: ['eslint:recommended'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module'
@@ -16,19 +14,19 @@ module.exports = {
     'no-unused-vars': 'warn',
     'no-console': 'off',
     'no-debugger': 'error',
-    
+
     // Best practices
-    'eqeqeq': 'error',
-    'curly': 'error',
+    eqeqeq: 'error',
+    curly: 'error',
     'no-eval': 'error',
     'no-implied-eval': 'error',
-    
+
     // Code style
-    'indent': ['error', 2],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
     'comma-dangle': ['error', 'never'],
-    
+
     // Node.js specific
     'no-process-exit': 'error',
     'no-path-concat': 'error'
@@ -41,4 +39,4 @@ module.exports = {
       }
     }
   ]
-}; 
+};
